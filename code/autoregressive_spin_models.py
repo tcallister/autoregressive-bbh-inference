57e388570681724a4040d95e17fa0bfedb36dc90
Thomas Callister <tcallister@Thomass-MacBook-Air.local>
2023-01-31 09:57:26 -0600

    Additional documentation of getData.py, removing unused effective spin flag

--- code/autoregressive_spin_models.py
+++ code/autoregressive_spin_models.py
@@ -453,3 +453,99 @@ def ar_Xeff_Xp(sampleDict,injectionDict,full_chi_data):
 
     # Tally log-likelihoods across our catalog
     numpyro.factor("logp",jnp.sum(log_ps))
+
+def ar_spinMagTilt_priorOnly(sampleDict,injectionDict,full_chi_data):
+
+    """
+    Implementation of a Gaussian effective spin distribution for inference within `numpyro`
+
+    Parameters
+    ----------
+    sampleDict : dict
+        Precomputed dictionary containing posterior samples for each event in our catalog
+    injectionDict : dict
+        Precomputed dictionary containing successfully recovered injections
+    """
+
+    all_chi_samples = full_chi_data['chi_allSamples']
+    ind_a01 = full_chi_data['ind_a01']
+    chi_deltas = full_chi_data['chi_deltas']
+    chi_deltas_low = chi_deltas[:ind_a01][::-1]
+    chi_deltas_high = chi_deltas[ind_a01:]
+
+    all_cost_samples = full_chi_data['cost_allSamples']
+    cost_deltas = full_chi_data['cost_deltas'][::-1]
+
+    ############################################################
+    # First sample the properties of our autoregressive process
+    # First get variance of the process
+    # We are imposing a steep power-law prior on this parameter
+    ar_chi_std = numpyro.sample("ar_chi_std",dist.HalfNormal(1.))
+    numpyro.factor("ar_chi_std_prior",ar_chi_std**2/2. - (ar_chi_std/0.75)**4/8.75)
+
+    # Finally the autocorrelation length
+    # Since the posterior for this parameter runs up against prior boundaries, sample in logit space
+    #log_ar_chi_tau = numpyro.sample("log_ar_chi_tau",dist.Normal(0.,1))
+    #ar_chi_tau = numpyro.deterministic("ar_chi_tau",10.**log_ar_chi_tau)
+    logit_ar_chi_tau = numpyro.sample("logit_ar_chi_tau",dist.Normal(0,logit_std))
+    ar_chi_tau,jac_ar_chi_tau = get_value_from_logit(logit_ar_chi_tau,0.2,2.)
+    numpyro.factor("p_ar_chi_tau",logit_ar_chi_tau**2/(2.*logit_std**2)-jnp.log(jac_ar_chi_tau))
+    numpyro.deterministic("ar_chi_tau",ar_chi_tau)
+    numpyro.factor("chi_regularization",-(ar_chi_std/jnp.sqrt(ar_chi_tau))**2/(2.*0.4**2))
+
+    # Sample an initial rate density at reference point
+    ln_f_chi_ref_unscaled = numpyro.sample("ln_f_chi_ref_unscaled",dist.Normal(0,1))
+    ln_f_chi_ref = ln_f_chi_ref_unscaled*ar_chi_std
+
+    # Generate forward steps
+    chi_steps_forward = numpyro.sample("chi_steps_forward",dist.Normal(0,1),sample_shape=(chi_deltas_high.size,))
+    chi_phis_forward = jnp.exp(-chi_deltas_high/ar_chi_tau)
+    chi_ws_forward = jnp.sqrt(1.-jnp.exp(-2.*chi_deltas_high/ar_chi_tau))*(ar_chi_std*chi_steps_forward)
+    final,ln_f_chis_high = lax.scan(build_ar1,ln_f_chi_ref,jnp.transpose(jnp.array([chi_phis_forward,chi_ws_forward]))) 
+    ln_f_chis = jnp.append(ln_f_chi_ref,ln_f_chis_high)
+
+    # Generate backward steps
+    chi_steps_backward = numpyro.sample("chi_steps_backward",dist.Normal(0,1),sample_shape=(chi_deltas_low.size,))
+    chi_phis_backward = jnp.exp(-chi_deltas_low/ar_chi_tau)
+    chi_ws_backward = jnp.sqrt(1.-jnp.exp(-2.*chi_deltas_low/ar_chi_tau))*(ar_chi_std*chi_steps_backward)
+    final,ln_f_chis_low = lax.scan(build_ar1,ln_f_chi_ref,jnp.transpose(jnp.array([chi_phis_backward,chi_ws_backward])))
+    ln_f_chis = jnp.append(ln_f_chis_low[::-1],ln_f_chis)
+
+    # Exponentiate and save
+    f_chis = jnp.exp(ln_f_chis)
+    f_chi_eventSorted = f_chis[full_chi_data['chi_reverseSorting']]
+    numpyro.deterministic("f_chis",f_chis)
+
+    ############################
+    # Construct AR1 process in cost
+    ############################
+
+    # First sample the properties of our autoregressive process
+    # First get variance of the process
+    ar_cost_std = numpyro.sample("ar_cost_std",dist.HalfNormal(1.))
+    numpyro.factor("ar_cost_std_prior",ar_cost_std**2/2. - (ar_cost_std/0.75)**4/8.75)
+
+    # Finally the autocorrelation length
+    #log_ar_cost_tau = numpyro.sample("log_ar_cost_tau",dist.Normal(0.,1.))
+    #ar_cost_tau = numpyro.deterministic("ar_cost_tau",10.**log_ar_cost_tau)
+    logit_ar_cost_tau = numpyro.sample("logit_ar_cost_tau",dist.Normal(0,logit_std))
+    ar_cost_tau,jac_ar_cost_tau = get_value_from_logit(logit_ar_cost_tau,0.3,4.)
+    numpyro.factor("p_ar_cost_tau",logit_ar_cost_tau**2/(2.*logit_std**2)-jnp.log(jac_ar_cost_tau))
+    numpyro.deterministic("ar_cost_tau",ar_cost_tau)
+    numpyro.factor("cost_regularization",-(ar_cost_std/jnp.sqrt(ar_cost_tau))**2/(2.*0.4**2))
+
+    ln_f_cost_ref_unscaled = numpyro.sample("ln_f_cost_ref_unscaled",dist.Normal(0,1))
+    ln_f_cost_ref = ln_f_cost_ref_unscaled*ar_cost_std
+
+    # Generate backward steps and prepend to reference value
+    cost_steps_backward = numpyro.sample("cost_steps_backward",dist.Normal(0,1),sample_shape=(cost_deltas.size,))
+    cost_phis_backward = jnp.exp(-cost_deltas/ar_cost_tau)
+    cost_ws_backward = jnp.sqrt(-jnp.expm1(-2.*cost_deltas/ar_cost_tau))*(ar_cost_std*cost_steps_backward)
+    final,ln_f_costs = lax.scan(build_ar1,ln_f_cost_ref,jnp.transpose(jnp.array([cost_phis_backward,cost_ws_backward])))
+    ln_f_costs = jnp.append(ln_f_costs[::-1],ln_f_cost_ref)
+
+    # Exponentiate and save
+    f_cost = jnp.exp(ln_f_costs)
+    f_cost_eventSorted = f_cost[full_chi_data['cost_reverseSorting']]
+    numpyro.deterministic("f_cost",f_cost)
+
--- code/getData.py
+++ code/getData.py
@@ -9,9 +9,35 @@ def reweighting_function_arlnm1_q(m1,m2,a1,a2,cost1,cost2,z,dVdz):
 
     """
     Function that computes weights under a fixed population similar to that preferred by
-    the ln(m1) AR1 analysis.
+    the ln(m1),q AR1 analysis. Preemptively reweighting posteriors and injections to this population
+    can help increase sampling efficiency when subsequently inferring the actual population distribution of BBHs
+
+    Parameters
+    ----------
+    m1 : `np.array`
+        Primary BBH masses (units of Msun)
+    m2 : `np.array`
+        Secondary BBH masses (units of Msun)
+    a1 : `np.array`
+        Primary spin magnitudes
+    a2 : `np.array`
+        Secondary spin magnitudes
+    cost1 : `np.array`
+        Cosines of primary spin-orbit tilts
+    cost2 : `np.array`
+        Cosines of secondary spin-orbit tilts
+    z : `np.array`
+        Redshifts
+    dVdz : `np.array`
+        Differential comoving volumes correspond to each redshift in `z`
+
+    Returns
+    -------
+    dP_dm1m2a1a2cost1cost2z : `np.array`
+        Probability density evaluated at each `(m1,m2,a1,a2,cost1,cost2,z)` under the target population
     """
 
+    # Hyperparameters characterizing target distribution
     kappa = 3.
     q_std = 0.5
     alpha = -2.5
@@ -19,6 +45,7 @@ def reweighting_function_arlnm1_q(m1,m2,a1,a2,cost1,cost2,z,dVdz):
     f_pl = 0.3
     f_peak_in_peak1 = 0.9
 
+    # Construct primary mass distribution
     p_m1 = np.zeros_like(m1)
     p_m1 += (1.-f_pl)*f_peak_in_peak1*np.exp(-(m1-10.)**2/(2.*0.8**2))/np.sqrt(2.*np.pi*0.8**2)
     p_m1 += (1.-f_pl)*(1.-f_peak_in_peak1)*np.exp(-(m1-35.)**2/(2.*2.**2.))/np.sqrt(2.*np.pi*2.**2)
@@ -30,16 +57,19 @@ def reweighting_function_arlnm1_q(m1,m2,a1,a2,cost1,cost2,z,dVdz):
     p_m1[m1<2.] = 0.
     p_m1[m1>100.] = 0.
 
+    # Compute mass ratio distribution and convert to a density over m2
     q = m2/m1
     p_q = np.exp(-(q-1)**2/(2.*q_std**2))*2./np.sqrt(2.*np.pi*q_std**2)
     p_m2 = p_q/m1
 
+    # Redshift distribution
     p_z = dVdz*(1.+z)**(kappa-1.)
     z_grid = np.linspace(0,2.3,1000)
     dVdz_grid = 4.*np.pi*Planck15.differential_comoving_volume(z_grid).to(u.Gpc**3*u.sr**(-1)).value
     norm = np.trapz(dVdz_grid*(1.+z_grid)**(kappa-1.))
     p_z /= norm
 
+    # Isotropic component spins
     p_a1 = 1.
     p_a2 = 1.
     p_cost1 = 1./2.
@@ -47,60 +77,67 @@ def reweighting_function_arlnm1_q(m1,m2,a1,a2,cost1,cost2,z,dVdz):
 
     return (p_m1*p_m2*p_a1*p_a2*p_cost1*p_cost2*p_z)
 
-def getInjections(sample_limit=10000,spin='component',reweight=False,weighting_function=reweighting_function_arlnm1_q):
+def getInjections(sample_limit=10000,reweight=False,weighting_function=reweighting_function_arlnm1_q):
 
     """
     Function to load and preprocess found injections for use in numpyro likelihood functions.
 
+    Parameters
+    ----------
+    sample_limit : int
+        If reweighting to a different reference population, number of found injections to use. No effect otherwise. Default 1e4
+    reweight : bool
+        If `True`, reweight injections to the reference population defined in `weighting_function`. Default `False`
+    weighting_function : func
+        Function defining new reference population to reweight to. No effect if `reweight=True`. Default `reweighting_function_arlnm1_q`
+
     Returns
-    -------
+    ------- 
     injectionDict : dict
-        Dictionary containing found injections
+        Dictionary containing found injections and associated draw probabilities, for downstream use in hierarchical inference
     """
 
+    # Load injections
     injectionFile = os.path.join(dirname,"injectionDict_FAR_1_in_1.pickle")
     injectionDict = np.load(injectionFile,allow_pickle=True)
 
+    # Convert all lists to numpy arrays
     for key in injectionDict:
         if key!='nTrials':
             injectionDict[key] = np.array(injectionDict[key])
 
+    # If reweighting injections to a new reference population...
     if reweight:
 
         print("REWEIGHTING")
 
+        # Extract injection parametetrs and old draw weights
         m1 = injectionDict['m1']
         m2 = injectionDict['m2']
         z = injectionDict['z']
         dVdz = injectionDict['dVdz']
+        a1 = injectionDict['a1']
+        a2 = injectionDict['a2']
+        cost1 = injectionDict['cost1']
+        cost2 = injectionDict['cost2']
+        pDraw = injectionDict['p_draw_m1m2z']*injectionDict['p_draw_a1a2cost1cost2']
 
-        if spin=='component':
-            a1 = injectionDict['a1']
-            a2 = injectionDict['a2']
-            cost1 = injectionDict['cost1']
-            cost2 = injectionDict['cost2']
-            pDraw = injectionDict['p_draw_m1m2z']*injectionDict['p_draw_a1a2cost1cost2']
-            p_new = weighting_function(m1,m2,a1,a2,cost1,cost2,z,dVdz)
+        # Define updated probabilities under new reference population
+        p_new = weighting_function(m1,m2,a1,a2,cost1,cost2,z,dVdz)
 
-        elif spin=='effective':
-            Xeff = injectionDict['Xeff']
-            Xp = injectionDict['Xp']
-            pDraw = injectionDict['p_draw_m1m2z']*injectionDict['p_draw_chiEff_chiP']
-            p_new = weighting_function(m1,m2,Xeff,Xp,z,dVdz)
-
+        # The ratio between new andn old draw weights will be used to resample found injections
         downselection_draw_weights = (p_new/pDraw)/injectionDict['nTrials']
         nEff_downselection = np.sum(downselection_draw_weights)**2/np.sum(downselection_draw_weights**2)
 
+        # Save new draw weights, which will take the place of `pDraw` above
+        # For convenience, put the weights entirely in `p_draw_m1m2z`, leaving `p_draw_a1a2cost1cost2` to be identically one
         new_pDraw = (p_new)/np.sum(downselection_draw_weights)
         injectionDict['downselection_Neff'] = nEff_downselection
         injectionDict['nTrials'] = sample_limit
         injectionDict['p_draw_m1m2z'] = new_pDraw
-
-        if spin=='component':
-            injectionDict['p_draw_a1a2cost1cost2'] = np.ones(new_pDraw.size)
-        elif spin=='effective':
-            injectionDict['p_draw_chiEff_chiP'] = np.ones(new_pDraw.size)
+        injectionDict['p_draw_a1a2cost1cost2'] = np.ones(new_pDraw.size)
         
+        # Randomly resample injections
         print(nEff_downselection)
         inds_to_keep = np.random.choice(np.arange(new_pDraw.size),size=sample_limit,replace=True,p=downselection_draw_weights/np.sum(downselection_draw_weights))
         for key in injectionDict:
@@ -109,63 +146,61 @@ def getInjections(sample_limit=10000,spin='component',reweight=False,weighting_f
 
     return injectionDict
 
-def getSamples(sample_limit=1000,bbh_only=True,spin='component',reweight=True,weighting_function=reweighting_function_arlnm1_q):
+def getSamples(sample_limit=2000,bbh_only=True,reweight=False,weighting_function=reweighting_function_arlnm1_q):
 
     """
     Function to load and preprocess BBH posterior samples for use in numpyro likelihood functions.
     
     Parameters
     ----------
-    sample_limit : int or None
-        If specified, will randomly downselect posterior samples, returning N=sample_limit samples per event (default None)
+    sample_limit : int
+        Number of posterior samples to retain for each event, for use in population inference (default 2000)
     bbh_only : bool
-        If true, will exclude samples for BNS, NSBH, and mass-gap events (default True)
+        If True, will exclude samples for BNS, NSBH, and mass-gap events (default True)
+    reweight : bool
+        If True, reweight posteriors to the reference population defined in `weighting_function`. Default `False`
+    weighting_function : func
+        Function defining new reference population to reweight to. No effect if `reweight=True`. Default `reweighting_function_arlnm1_q`
 
     Returns
     -------
     sampleDict : dict
-        Dictionary containing posterior samples
+        Dictionary containing posterior samples, for downstream use in hierarchical inference
     """
 
-    # Dicts with samples:
+    # Load dictionary with preprocessed posterior samples
     sampleFile = os.path.join(dirname,"sampleDict_FAR_1_in_1_yr.pickle")
     sampleDict = np.load(sampleFile,allow_pickle=True)
 
+    # Remove non-BBH events, if desired
     non_bbh = ['GW170817','S190425z','S190426c','S190814bv','S190917u','S200105ae','S200115j']
     if bbh_only:
         for event in non_bbh:
             print("Removing ",event)
             sampleDict.pop(event)
 
-    mMin = 0.
+    # Loop across events
     for event in sampleDict:
 
+        # If we are preemptively reweighting to a new reference prior...
         if reweight:
 
+            # Extract samples
             m1 = np.array(sampleDict[event]['m1'])
             m2 = np.array(sampleDict[event]['m2'])
             z = np.array(sampleDict[event]['z'])
             dVdz = np.array(sampleDict[event]['dVc_dz'])
+            a1 = np.array(sampleDict[event]['a1'])
+            a2 = np.array(sampleDict[event]['a2'])
+            cost1 = np.array(sampleDict[event]['cost1'])
+            cost2 = np.array(sampleDict[event]['cost2'])
+            prior = np.array(sampleDict[event]['z_prior'])
 
-            if spin=='component':
+            # Define new population prior, and for convenience put it into the pre-existing `z_prior` field
+            p_new = weighting_function(m1,m2,a1,a2,cost1,cost2,z,dVdz)
+            sampleDict[event]['z_prior'] = p_new
 
-                a1 = np.array(sampleDict[event]['a1'])
-                a2 = np.array(sampleDict[event]['a2'])
-                cost1 = np.array(sampleDict[event]['cost1'])
-                cost2 = np.array(sampleDict[event]['cost2'])
-                prior = np.array(sampleDict[event]['z_prior'])
-                p_new = weighting_function(m1,m2,a1,a2,cost1,cost2,z,dVdz)
-                sampleDict[event]['z_prior'] = p_new
-
-            elif spin=='effective':
-
-                Xeff = np.array(sampleDict[event]['Xeff'])
-                Xp = np.array(sampleDict[event]['Xp'])
-                prior = np.array(sampleDict[event]['z_prior'])*np.array(sampleDict[event]['joint_priors'])
-                p_new = weighting_function(m1,m2,a1,a2,cost1,cost2,z,dVdz)
-                sampleDict[event]['z_prior'] = p_new
-                sampleDict[event]['joint_priors'] = np.ones(Xeff.size)
-
+            # Define weights for random downselection below
             draw_weights = p_new/prior
             draw_weights /= np.sum(draw_weights)
             neff = np.sum(draw_weights)**2/np.sum(draw_weights**2)
@@ -173,21 +208,18 @@ def getSamples(sample_limit=1000,bbh_only=True,spin='component',reweight=True,we
             if neff<2.*sample_limit:
                 print(event,neff)
 
+        # If not reweighting...
         else:
             
+            # Uniform draw weights
             draw_weights = np.ones(sampleDict[event]['m1'].size)/sampleDict[event]['m1'].size
 
         sampleDict[event]['downselection_Neff'] = np.sum(draw_weights)**2/np.sum(draw_weights**2)
 
+        # Randomly downselect to the desired number of samples       
         inds_to_keep = np.random.choice(np.arange(sampleDict[event]['m1'].size),size=sample_limit,replace=True,p=draw_weights)
         for key in sampleDict[event].keys():
             if key!='downselection_Neff':
                 sampleDict[event][key] = sampleDict[event][key][inds_to_keep]
 
-        new_mMin = np.min(sampleDict[event]['m1'])
-        if new_mMin>mMin:
-            mMin = new_mMin
-
-    print("!!!!!!",mMin)
-        
     return sampleDict
