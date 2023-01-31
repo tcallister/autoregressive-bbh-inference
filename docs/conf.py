51e29ca3ce9824c5c9ba86b882796e71c8bb75e2
Thomas Callister <tcallister@Thomass-MacBook-Air.local>
2023-01-31 15:09:53 -0600

    Adding empty docs setup

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'autoregressive-bbh-inference'
+copyright = '2023, Tom Callister & Will Farr'
+author = 'Tom Callister & Will Farr'
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../code/'))
+import sphinx_rtd_theme
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_rtd_theme','sphinx.ext.autosectionlabel', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.githubpages','sphinx.ext.napoleon','sphinx.ext.viewcode']
+templates_path = ['templates']
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['static']
+html_theme_options = {'body_max_width':'100%'}
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. autoregressive-bbh-inference documentation master file, created by
+   sphinx-quickstart on Tue Jan 31 15:03:20 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to autoregressive-bbh-inference's documentation!
+========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
