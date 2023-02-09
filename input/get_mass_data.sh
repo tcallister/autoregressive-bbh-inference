925d541be991d74a123c98a5c0bb5fe2c50c50af
Thomas Callister <tcallister@Thomass-MacBook-Air.local>
2023-02-09 15:37:36 -0600

    Removing deprecated script from input directory, adding placeholder

--- input/get_mass_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#scp thomas.callister@ldas-grid.ligo.caltech.edu:/home/thomas.callister/CBC/o3b-population-data/analyses/PowerLawPeak/o1o2o3_mass_c_iid_mag_iid_tilt_powerlaw_redshift_result.json .
-#scp thomas.callister@ldas-grid.ligo.caltech.edu:/home/thomas.callister/CBC/o3b-population-data/figure_scripts/make_plpeak_mass_spectrum.py .
-#scp thomas.callister@ldas-grid.ligo.caltech.edu:/home/thomas.callister/CBC/o3b-population-data/analyses/PowerLawPeak/o1o2o3_mass_c_iid_mag_iid_tilt_powerlaw_redshift_mass_data.h5 .
-scp thomas.callister@ldas-grid.ligo.caltech.edu:/home/thomas.callister/CBC/o3b-population-data/analyses/GaussianSpin/posteriors_gaussian_spin_samples_FAR_1_in_1.json .
