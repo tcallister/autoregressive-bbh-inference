3ee114d518566966a6e700ca97d94a1129cfb86e
Thomas Callister <tcallister@midway3-login4.rcc.local>
2023-05-11 16:32:59 -0500

    Misc updates

--- code/run_ar_chi_cost.py
+++ code/run_ar_chi_cost.py
@@ -1,6 +1,6 @@
 import numpyro
 import sys
-nChains = 1#3
+nChains = 3
 numpyro.set_host_device_count(nChains)
 from numpyro.infer import NUTS,MCMC,init_to_median,init_to_value
 from jax import random
@@ -98,8 +98,7 @@ init_values = {
 kernel = NUTS(ar_spinMagTilt,\
                 dense_mass=[("ar_chi_std","logit_ar_chi_tau"),("ar_cost_std","logit_ar_cost_tau")],
                 init_strategy=init_to_value(values=init_values),target_accept_prob=0.9)
-#mcmc = MCMC(kernel,num_warmup=500,num_samples=1500,num_chains=nChains)
-mcmc = MCMC(kernel,num_warmup=300,num_samples=600,num_chains=nChains)
+mcmc = MCMC(kernel,num_warmup=500,num_samples=1500,num_chains=nChains)
 
 # Choose a random key and run over our model
 rng_key = random.PRNGKey(347)
@@ -111,7 +110,7 @@ mcmc.print_summary()
 data = az.from_numpyro(mcmc)
 #az.to_netcdf(data,"/mnt/ceph/users/tcallister/autoregressive-bbh-inference-data/final-ar_chi_cost.cdf")
 #np.save('/mnt/ceph/users/tcallister/autoregressive-bbh-inference-data/final-ar_chi_cost_data.npy',full_chi_data)
-az.to_netcdf(data,"./../data/ar_chi_cost_entropy.cdf")
-np.save('./../data/ar_chi_cost_data_entropy.npy',full_chi_data)
+az.to_netcdf(data,"/project2/kicp/tcallister/autoregressive-bbh-inference-data/ar_chi_cost.cdf")
+np.save('/project2/kicp/tcallister/autoregressive-bbh-inference-data/ar_chi_cost_data.npy',full_chi_data)
 
 
