34c3fe0949a7e822d65f31f04886fc90accc1776
Thomas Callister <tcallister@Thomass-MacBook-Air.local>
2023-06-26 11:50:15 -0500

    Adding mock data run scripts

---
+++ mock-data/process_mock_populations.py
@@ -0,0 +1,57 @@
+import arviz as az
+import numpy as np
+import sys
+import h5py
+sys.path.append('./../code/')
+from utilities import *
+
+def process_and_save(cdf_name):
+
+    # Load inference results
+    inference_data = az.from_netcdf(cdf_name)
+    samps = az.extract(inference_data,var_names=["R20","fs","ar_std","ar_tau",\
+                    "nEff_inj_per_event","min_log_neff"])
+
+    R_ref = samps.R20.values
+    fs = samps.fs.values
+    ar_std = samps.ar_std.values
+    ar_tau = samps.ar_tau.values
+    nEff_inj_per_event = samps.nEff_inj_per_event.values
+    min_log_neff = samps.min_log_neff.values
+
+    # Also extract complete set of spin magnitude and tilt values over which AR process is defined
+    sample_file = "{0}_data.npy".format(cdf_name.split(".")[0])
+    sample_data = np.load(sample_file,allow_pickle=True)[()]
+    chi_values = sample_data['all_samples']
+
+    # For ease of plotting and storage, coarse-grain by a factor of 20
+    fs_reduced = fs[::50,:]
+    chi_values_reduced = chi_values[::50]
+
+    # Compute rate density over mock spin space
+    dR_dchis = R_ref[np.newaxis,:]*fs_reduced
+
+    # Create hdf5 file and write posterior samples
+    hfile_name = "{0}_summary.hdf".format(cdf_name.split(".")[0])
+    hfile = h5py.File(hfile_name,'w')
+    posterior = hfile.create_group('posterior')
+    posterior.create_dataset('chis',data=chi_values_reduced)
+    posterior.create_dataset('fs',data=fs_reduced)
+    posterior.create_dataset('dR_dchis',data=dR_dchis)
+    posterior.create_dataset('ar_std',data=ar_std)
+    posterior.create_dataset('ar_tau',data=ar_tau)
+    posterior.create_dataset('R_ref',data=R_ref)
+    posterior.create_dataset('nEff_inj_per_event',data=nEff_inj_per_event)
+    posterior.create_dataset('min_log_neff',data=min_log_neff)
+
+    # Add some metadata
+    hfile.attrs['Created_by'] = "process_mock_populations.py"
+    hfile.attrs['Downloadable_from'] = "10.5281/zenodo.7600141"
+    hfile.attrs['Source_code'] = "https://github.com/tcallister/autoregressive-bbh-inference"
+
+    hfile.close()
+
+process_and_save("ar_gaussian_varyingUncertainty_069.cdf")
+process_and_save("ar_spike_varyingUncertainty_069.cdf")
+process_and_save("ar_gaussian_spike_varyingUncertainty_069.cdf")
+process_and_save("ar_half_normal_varyingUncertainty_069.cdf")
---
+++ mock-data/run_half_normal_varyingUncertainty_069.py
@@ -0,0 +1,86 @@
+import numpyro
+nChains = 1
+numpyro.set_host_device_count(nChains)
+from numpyro.infer import NUTS,MCMC,init_to_value,init_to_median
+from jax import random
+import jax.numpy as jnp
+from jax.config import config
+config.update("jax_enable_x64", True)
+import arviz as az
+import numpy as np
+np.random.seed(111)
+from autoregressive_model import ar_model
+import sys
+sys.path.append("./../code/")
+from utilities import compute_prior_params
+
+"""
+This script runs an autoregressive population inference on the mock data in `half_normal_samples_varyingUncertainty.npy`,
+which is itself created by the jupyter notebook `gen_fake_samples.ipynb`.
+In particular, it will analyze the first 69 mock posteriors in this data set, to mirror the size of our actual GWTC-3 catalog.
+"""
+
+# Get dictionaries holding posterior samples
+# Restrict to the first 69 mock events
+sampleDict = np.load('half_normal_samples_varyingUncertainty.npy',allow_pickle=True)[()]
+sampleDict = {i:sampleDict[i] for i in range(69)}
+
+# Instantiate array to hold all combined posterior samples
+total_Samples = 0
+all_samples = np.array([])
+
+# Loop across individual events
+for key in sampleDict:
+
+    # Append samples together and compute range of indices holding this event's samples
+    nSamples = len(sampleDict[key]['samps'])
+    all_samples = np.append(all_samples,sampleDict[key]['samps'])
+    sampleDict[key]['ar_indices'] = np.arange(total_Samples,total_Samples+nSamples)
+    total_Samples += nSamples
+
+# Assume no selection effects.
+# At the same time, we want to realistically mirror how found injections influence the AR(1) inference,
+# so we will still characterize selection effects by randomly "detecting" a set of mock injections
+# spread uniformly throughout parameter space
+injectionDict = {'inj':np.random.random(10000)*2.-1.,'p_draw':np.ones(10000)*(1./2.),'nTrials':10000.}
+
+# Read out values from injection set
+nInjections = len(injectionDict['inj'])
+all_samples = np.append(all_samples,injectionDict['inj'])
+injection_indices = np.arange(total_Samples,total_Samples+nInjections)
+total_Samples += nInjections
+
+# Get sorting information and differences between adjacent samples
+sorting = np.argsort(all_samples)
+sorting_into_events = np.argsort(sorting)
+deltas = np.diff(all_samples[sorting])
+
+# Package into dictionary
+full_data = {'all_samples':all_samples[sorting],
+                'deltas':deltas,
+                'reverseSorting':sorting_into_events,
+                'ind_ref':np.argmin((all_samples[sorting])**2.),
+                'injections_from_allSamples':injection_indices}
+
+# Compute hyperparameter prior constraints
+dR_max = 100
+dR_event = 2
+N = 69
+DeltaX = 2.
+std_std,ln_tau_mu,ln_tau_std,regularization_std = compute_prior_params(dR_max,dR_event,DeltaX,N)
+
+# Set up NUTS sampler over our likelihood
+kernel = NUTS(ar_model,init_strategy=init_to_median)
+mcmc = MCMC(kernel,num_warmup=600,num_samples=750,num_chains=nChains)
+
+# Choose a random key and run over our model
+rng_key = random.PRNGKey(111)
+rng_key,rng_key_ = random.split(rng_key)
+mcmc.run(rng_key_,sampleDict,injectionDict,full_data,std_std=std_std,ln_tau_mu=ln_tau_mu,ln_tau_std=ln_tau_std,regularization_std=regularization_std)
+mcmc.print_summary()
+
+# Save out data
+data = az.from_numpyro(mcmc)
+az.to_netcdf(data,"ar_half_normal_varyingUncertainty_069.cdf")
+np.save('ar_half_normal_varyingUncertainty_069_data.npy',full_data)
+
